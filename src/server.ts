81c088bf773f1f55777ab71a53d4537a8ac56a0c
Alisson Lee Martins <165162969+LeeAlisson@users.noreply.github.com>
2025-11-05 14:05:55 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,23 +9,25 @@ import bookingRoutes from "./routes/booking.routes";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger";
 
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/auth", authRoutes);
+app.use("/labs", labRoutes);
+app.use("/rooms", roomRoutes);
+app.use("/bookings", bookingRoutes);
+
 AppDataSource.initialize()
   .then(() => {
-    const app = express();
-    app.use(
-      cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:3000",
-        credentials: true,
-      })
-    );
-    app.use(express.json());
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-    app.use("/auth", authRoutes);
-    app.use("/labs", labRoutes);
-    app.use("/rooms", roomRoutes);
-    app.use("/bookings", bookingRoutes);
-
-    const PORT = process.env.PORT || 3001;
-    app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+    console.log("✅ Data Source initialized!");
   })
   .catch((err) => console.error("Error initializing data source:", err));
+
+export default app;
